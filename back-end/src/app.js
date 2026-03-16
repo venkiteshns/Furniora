235b61c96ec14adc3b974b0b72f199d798dcb1fb
Venkitesh N S <software.venkitesh.in@gmail.com>
2026-03-16 20:24:15 +0530

    Backend hosted in render | Frontend hosted in vercel | configured the backend url in frontend vite.config.js | Cors setup completed for frontend url in app.js

--- back-end/src/app.js
+++ back-end/src/app.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express()
 
@@ -7,16 +8,25 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
+app.use(
+  cors({
+    origin: "https://furniora-five.vercel.app",
+    credentials: true,
+  })
+);
 
 import router from './routes/user.js'
 
 app.use('/',router)
 
 
-app.use((err,req,res,next) => {
-    if(err) {
-        console.log("server error occoured",err);
-    }
-})
+app.use((err, req, res, next) => {
+  console.error("Server error occurred:", err);
+
+  res.status(err.status || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
 
 export default app;
--- front-end/vite.config.js
+++ front-end/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   server:{
     proxy:{
-      "/user": "http://localhost:7000"
+      "/user": "https://the-royal-furnitures.onrender.com"
     }
   },
   plugins: [react()],
